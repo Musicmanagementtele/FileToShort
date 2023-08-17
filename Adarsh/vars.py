d2a4bb88ba48db972231050c982ea8105a0a5301
Musicmanagementtele <121627238+Musicmanagementtele@users.noreply.github.com>
2023-08-17 14:04:29 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -20,17 +20,17 @@ class Var(object):
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "BOTTOM")
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "885675538 1471469091").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1471469091").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     SHORTENER_API = str(getenv('SHORTENER_API', '02c93c55c4567035a37ffc32731d8f0e1c530f98'))
     SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'tnshort.net'))
-    FDQN = str(getenv('FDQN', 'https://qtmvelinkz.onrender.com'))
+    FDQN = str(getenv('FDQN', ''))
 
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('Owner_of_qtmve'))
+    OWNER_USERNAME = str(getenv('thavarajtj'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', 'qtmvelinkz'))
+        APP_NAME = str(getenv('APP_NAME', ''))
     
     else:
         ON_HEROKU = False
@@ -41,5 +41,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://hillsking1222:kuraman1@kumaran1.d6ahc05.mongodb.net/?retryWrites=true&w=majority"))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "@qtmve_linkzZz"))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "SAM_DUB_LEZHa"))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
